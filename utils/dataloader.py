e3a6b7d182fbf6e222669a4e6ed8c037d4c5c575
Odin Hoff Gardaa <odinhg@gmail.com>
2023-02-18 12:49:21 +0100

    added custom dataloader with split and normalization

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,28 +1,25 @@
 import torch
 from torch.utils.data.dataset import Dataset
-from torchvision import transforms
-from os import listdir
-from os.path import isfile, join
+from torch.utils.data import DataLoader, random_split
+from os.path import isfile 
 import pandas as pd
 import numpy as np
-#from PIL import Image
-#from tqdm import tqdm
-#from itertools import permutations
-#from random import sample, choice
 
 class TrafficVolumeDataSet(Dataset):
     def __init__(self, datafile):
         self.datafile = datafile
+        assert isfile(self.datafile), f"Error: Data file {self.datafile} not found! Please run preprocess_data.py first."
+        print(f"Loading traffic data from {self.datafile}...")
         self.df = pd.read_pickle(self.datafile)
         self.len = len(self.df) - 1 # Since there is no row after the last row.
         self.column_names = self.df.columns
 
     def __getitem__(self, index):
         # Return two consecutive rows of traffic data (and date / time)
-        # Replace NaNs with -1 hoping that the model will learn that this means missing data
+        # Replace NaNs with -1 
         data_now = self.df.iloc[index].replace(np.nan, -1)
         data_next = self.df.iloc[index + 1].replace(np.nan, -1)
-        # Extracting month, weekday and hour
+
         month = getattr(data_now, "name").month
         weekday = getattr(data_now, "name").weekday()
         hour = getattr(data_now, "name").hour
@@ -34,39 +31,43 @@ class TrafficVolumeDataSet(Dataset):
     def __len__(self):
         return self.len
 
-dataset = TrafficVolumeDataSet("../data/time_series_data.pkl")
-print(len(dataset))
-for i in range(796,800):
-    print(dataset[i])
+def mean_std(subset):
+    indices = subset.indices
+    dataset = subset.dataset.df.iloc[indices, :]
+    return (dataset.mean(), dataset.std())
 
-"""
-def FlowCamDataLoader(class_names, image_size = (300, 300), val = 0.1, test = 0.2, batch_size = 32, split=True):
-    dataset = FlowCamDataSet(class_names, image_size)
-    num_workers = 8
-    if not split:
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-        return dataloader
+def normalize(subset, mean, std):
+    indices = subset.indices
+    subset.dataset.df.iloc[indices, :] = (subset.dataset.df.iloc[indices, :] - mean) / std
+    return subset
 
-    #Split into train and test data
+def TrafficVolumeDataLoader(datafile, val=0.1, test=0.1, batch_size=32, num_workers=4, random_seed=0, normalize_data=True):
+    dataset = TrafficVolumeDataSet(datafile)
+    
+    # Split the dataset into training, validation and testing data
     val_size = int(val * len(dataset))
     test_size = int(test * len(dataset))
     train_size = len(dataset) - val_size - test_size
-    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(420))
+    generator = torch.Generator().manual_seed(random_seed)
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size], generator=generator)
 
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    # Normalize dataset with statistics computed on the training daga (to prevent data leakage)
+    if normalize_data:
+        print("Normalizing datasets...")
+        mean, std = mean_std(train_dataset)
+        train_dataset = normalize(train_dataset, mean, std)
+        val_dataset = normalize(val_dataset, mean, std)
+        test_dataset = normalize(test_dataset, mean, std)
     
-    print(f"Training data: {len(train_dataloader)*batch_size} images.")
-    print(f"Validation data: {len(val_dataloader)*batch_size} images.")
-    print(f"Test data: {len(test_dataloader)*batch_size} images.")
+    # Create dataloaders
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    
+    print(f"Training data: {len(train_dataloader)*batch_size} rows.")
+    print(f"Validation data: {len(val_dataloader)*batch_size} rows.")
+    print(f"Test data: {len(test_dataloader)*batch_size} rows.")
 
-    return  {
-            "train_dataloader"  : train_dataloader, 
-            "val_dataloader"    : val_dataloader, 
-            "test_dataloader"   : test_dataloader, 
-            "train_dataset"     : train_dataset, 
-            "val_dataset"       : val_dataset, 
-            "test_dataset"      : test_dataset
-            }
-"""
+    return (train_dataloader, val_dataloader, test_dataloader)
+
+TrafficVolumeDataLoader("../data/time_series_data.pkl")
