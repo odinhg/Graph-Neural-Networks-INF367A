efc666c32a872fc17721d8bd56eb65c73e1ab98c
Odin Hoff Gardaa <odinhg@gmail.com>
2023-02-21 18:36:17 +0100

    added graph and screenshots of graph on map

--- geo.py
+++ geo.py
@@ -6,25 +6,55 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 def distance(s1, s2):
-    # Compute distance between two stations
-    # Only needed because *some* people don't believe the earth is flat
     return geodesic(s1, s2).km
 
+# Only consider traffic stations included in our dataset (where some stations are excluded)
+#included_ids = pd.read_pickle("data/time_series_test.pkl").columns
 stations = pd.read_csv("data/traffic_stations.csv")
+#stations = stations.loc[stations["id"].isin(included_ids), :]
+
 coordinates = stations.loc[:, ["latitude", "longitude"]]
 distances = pdist(coordinates, metric=distance)
+
+
 
 fig = go.Figure(
         go.Scattermapbox(
             lat = stations.loc[:, "latitude"],
             lon = stations.loc[:, "longitude"],
             mode = "text+markers",
-            text = stations.loc[:, "id"],
+            text = list(range(0,len(stations))), #stations.loc[:, "id"],
             textposition = "top center",
             marker_size = 12,
         )
     )
-fig.update_layout(title_text = "Traffic Stations", title_x = 0.5)
+
+# Add edges to graph
+graph_df = pd.read_pickle("data/graph.pkl")
+for i in range(0, len(graph_df)):
+    for j in range(i, len(graph_df)):
+        if graph_df.iloc[i,j] == 1:
+            #station_1 = graph_df.columns[i]
+            #station_2 = graph_df.columns[j]
+            fig.add_trace(
+                    go.Scattermapbox(
+                        lat = [stations.iloc[i]["latitude"], stations.iloc[j]["latitude"]],
+                        lon = [stations.iloc[i]["longitude"], stations.iloc[j]["longitude"]],
+                        mode = "lines",
+                    )
+                )
+
+"""
+fig.add_trace(
+        go.Scattermapbox(
+            lat = [stations.iloc[0]["latitude"], stations.iloc[2]["latitude"]],
+            lon = [stations.iloc[0]["longitude"], stations.iloc[2]["longitude"]],
+            mode = "lines",
+        )
+    )
+"""
+
+fig.update_layout(title_text = "Traffic Stations", title_x = 0.5, showlegend=False)
 fig.update_layout(mapbox_style="open-street-map")
 fig.show()
 
